5a3f8467e86d7096dc7403dc762986b0db38366f
gsalinasphys <77408096+gsalinasphys@users.noreply.github.com>
2022-07-16 16:51:55 +0200

    Filtering (#3)
    * Added filtering script
    
    * Added docstring for arxiv_filter

---
+++ dataset_filter.py
@@ -0,0 +1,37 @@
+# Filtering arxiv dataset
+from pathlib import Path
+from typing import List, Tuple
+
+import pandas as pd
+
+
+def arxiv_filter(
+    directory: str, yr_range: Tuple[float, float], categories: List[str] = None
+) -> pd.DataFrame:
+    """Filters an arXiv database from a directory, based on the year of last update
+    and categories.
+    Files should be divided into chunks named in the format "chunk*.json".
+    """
+    files = Path(directory).glob("chunk*.json")
+
+    frames = []
+    for file in files:
+        achunk = pd.read_json(file)
+
+        achunk["year"] = [int(date[:4]) for date in achunk["update_date"]]
+
+        if categories:
+            achunk_filter = achunk.loc[
+                (achunk["year"] >= yr_range[0])
+                & (achunk["year"] <= yr_range[1])
+                & (achunk["categories"].isin(categories))
+            ].copy()
+        else:
+            achunk_filter = achunk.loc[
+                (achunk["year"] >= yr_range[0]) & (achunk["year"] <= yr_range[1])
+            ].copy()
+        achunk_filter.drop("year", axis=1, inplace=True)
+
+        frames.append(achunk_filter)
+
+    return pd.concat(frames)
