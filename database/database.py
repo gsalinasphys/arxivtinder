39bb9ee1c2c660a5a2a1ef5fe17b2faa43092e39
pdpano <aurichio.v@gmail.com>
2022-08-30 17:28:08 +0100

    No echo

--- database/database.py
+++ database/database.py
@@ -2,4 +2,4 @@ from sqlalchemy import create_engine
 
 DATABASE_URI = "sqlite+pysqlite:///static/sqlite.db"
 
-engine = create_engine(DATABASE_URI, echo=True, future=True)
+engine = create_engine(DATABASE_URI, future=True)
