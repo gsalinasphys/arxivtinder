c841d0f674b908b1df9fbe49b534fc14ff36e3d9
Vinicius Henrique Aurichio <aurichio.v@gmail.com>
2022-09-20 12:47:22 +0100

    Update db (#11)
    * Add relations
    
    * Create initial DB for real
    
    * Update field name
    
    * No echo
    
    * Ensure non-nullable
    
    * Update migration

--- database/orm/article.py
+++ database/orm/article.py
@@ -1,8 +1,41 @@
-from sqlalchemy import Column, String
+from sqlalchemy import BLOB, Column, Date, ForeignKey, String
+from sqlalchemy.orm import deferred, relationship
 
 from .base import Base
+
+
+class CategoryTagArticle(Base):
+    __tablename__ = "category_tags_articles"
+    category_tag_id = Column(ForeignKey("category_tags.tag"), primary_key=True)
+    article_id = Column(ForeignKey("articles.id"), primary_key=True)
 
 
 class Article(Base):
     __tablename__ = "articles"
     id = Column(String, primary_key=True)
+    abstract = Column(String, nullable=False)
+    abstract_embedding = deferred(Column(BLOB(2**24 - 1)))
+    title = Column(String, nullable=False)
+    submitter = Column(String)
+    journal_ref = Column(String)
+    doi = Column(String)
+    authors = Column(String)
+    updated_at = Column(Date, nullable=False)
+
+    tags = relationship(
+        "CategoryTag",
+        secondary=CategoryTagArticle.__tablename__,
+        back_populates="articles",
+    )
+
+
+class CategoryTag(Base):
+    __tablename__ = "category_tags"
+    tag = Column(String, primary_key=True)
+    name = Column(String, nullable=False)
+
+    articles = relationship(
+        "Article",
+        secondary=CategoryTagArticle.__tablename__,
+        back_populates="tags",
+    )
