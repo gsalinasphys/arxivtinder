350f23d06f2f163ce470c1dd7efc70539ac80ffb
Gustavo Salinas <gsalinas.phys@gmail.com>
2022-10-07 20:05:58 +0200

    Added category dropdown, not working yet

--- tinder/app.py
+++ tinder/app.py
@@ -1,5 +1,6 @@
 import os
 
+import numpy as np
 from flask import Flask, render_template, request, url_for
 
 from utils.semantic_search import (find_row_numbers, get_df, get_embeddings,
@@ -9,21 +10,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", categories=categories)
 
 
 @app.route("/top", methods=["POST"])
 def top():
-    path_to_dataset = "../static/arxiv-clean.json"
-    arxiv_df = get_df(path_to_dataset)
-
-    filename, _ = os.path.splitext(path_to_dataset)
-    corpus_ids = arxiv_df["id"].values
-    embeddings = get_embeddings(filename)
-
     query_ids = request.form.get("id").split()
     if not query_ids or not all(query_id in corpus_ids for query_id in query_ids):
         return render_template("failure.html")
+
+    
 
     rows = find_row_numbers(query_ids, corpus_ids)
     all_tophits = top_hits(embeddings, rows, int(request.form.get("n_recommendations")))
@@ -42,5 +38,13 @@ def top():
         "success.html",
         query_ids=query_ids,
         n_queries=len(query_ids),
-        hits_formatted=hits_formatted,
+        hits_formatted=hits_formatted
     )
+
+path_to_dataset = "../static/arxiv-clean.json"
+arxiv_df = get_df(path_to_dataset)
+corpus_ids = arxiv_df["id"].values
+categories = sorted(list(set([category for categories in arxiv_df["categories"].str.split().values for category in categories])))
+
+filename, _ = os.path.splitext(path_to_dataset)
+embeddings = get_embeddings(filename)
